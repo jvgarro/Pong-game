d1de3219102e3a73a57d22e5569d105515e5ffeb
Clayre1 <josuevillalobos19@gmail.com>
2022-07-12 20:58:20 -0600

    Arreglo de bugs y cambio de colores

--- pong.py
+++ pong.py
@@ -1,133 +1,137 @@
 import pygame
 import sys
 from pygame.locals import *
+import random
+
 
 class Game():
-
     def __init__(self):
-
-        self.CLOCK = None
-        self.SCREEN = None
-        self.my_font = None
-
-        self.BLACK = (0, 0, 0)
-        self.WHITE = (200, 200, 200)
+        pygame.init()
+        pygame.font.init()
+        self.my_font = pygame.font.Font('Font/Pixeltype.ttf', 50)
+        self.begin_text = 'Press SPACE to start'
         self.WINDOW_HEIGHT = 420
         self.WINDOW_WIDTH = 420
-        self.BLOCSIZE = 20 #Set the size of the grid block
-        self.start = True
-
-        self.x_pong1 = 20
-        self.y_pong1 = 180
-        self.x_pong2 = 380
-        self.y_pong2 = 180
-        self.x_ball_pos = 200
-        self.y_ball_pos = 200
-        self.x_velocity = 1
-        self.y_velocity = 0
-        self.score1 = 0
-        self.score2 = 0
-
-        self.main()
-    
-    def main(self):
-        pygame.init()
+        self.BLOCSIZE = 20  # Set the size of the grid block
+        self.start = False
         self.CLOCK = pygame.time.Clock()
-        self.SCREEN = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
-        pygame.font.init()
-        self.my_font = pygame.font.Font('Font/Pixeltype.ttf',50)
+        self.SCREEN = pygame.display.set_mode(
+            (self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
+
+        # X,Y arrays for positioning the objects
+        self.pad1 = [0, 180]
+        self.pad2 = [410, 180]
+        self.ball = [200, 200]
+        self.velocity = [10, 1]
+
+        # Scores for player 1 and 2
+        self.scores = [0, 0]
+        self.main()
+
+    def main(self):
 
         while True:
-            self.CLOCK.tick(60)
-            self.SCREEN.fill(self.BLACK)
-            self.objects()
-            text_surface = self.my_font.render(str(self.score1) + ' :SCORE: ' + str(self.score2), False, 'White')
-            self.SCREEN.blit(text_surface, (120, 0))
-
-            self.endGame()
-            
-            self.controllers()
-            
-            self.collisions()
-            
-            self.checkScreenBoundaries()
-            
-            self.x_ball_pos += self.x_velocity
-            self.y_ball_pos += self.y_velocity      
-            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.start = True
+                        self.begin_text = ' '
+
+            self.CLOCK.tick(60)
+            self.SCREEN.fill('gray20')
+            self.objects()
+            score_surface = self.my_font.render(
+                str(self.scores[0]) + ' :SCORE: ' + str(self.scores[1]), False, 'White')
+            self.SCREEN.blit(score_surface, (120, 0))
+            
+            begin_surface = self.my_font.render(
+                str(self.begin_text), False, 'White')
+            self.SCREEN.blit(begin_surface, (60, 120)) 
+
+            if self.start == True:
+
+                if self.scores[0] == 10:
+                    self.endGame(self.scores[0], "Player 1")
+                if self.scores[1] == 10:
+                    self.endGame(self.scores[1], "Player 2")
+                    
+
+                self.controllers()
+                self.collisions()
+                self.checkScreenBoundaries()
+
+                self.ball[0] += self.velocity[0]
+                self.ball[1] += self.velocity[1]
+
             pygame.display.update()
-    
+
     def objects(self):
-        pygame.draw.rect(self.SCREEN, self.WHITE, (self.x_ball_pos, self.y_ball_pos, self.BLOCSIZE, self.BLOCSIZE))
-        pygame.draw.rect(self.SCREEN, self.WHITE, (self.x_pong1, self.y_pong1, self.BLOCSIZE, self.BLOCSIZE*3))        
-        pygame.draw.rect(self.SCREEN, self.WHITE, (self.x_pong2, self.y_pong2, self.BLOCSIZE, self.BLOCSIZE*3))
+        global ballRect, pad1Rect, pad2Rect
+        ballRect = Rect(self.ball[0], self.ball[1],
+                        self.BLOCSIZE, self.BLOCSIZE)
+        pad1Rect = Rect(self.pad1[0], self.pad1[1], 10, self.BLOCSIZE*3)
+        pad2Rect = Rect(self.pad2[0], self.pad2[1], 10, self.BLOCSIZE*3)
+
+        pygame.draw.rect(self.SCREEN, 'gold', ballRect)
+        pygame.draw.rect(self.SCREEN, 'mediumblue', pad1Rect)
+        pygame.draw.rect(self.SCREEN, 'orangered', pad2Rect)
 
     def controllers(self):
-        keys = pygame.key.get_pressed()   
-        if keys[pygame.K_w] and self.y_pong1 > 0:
-            self.y_pong1 -= 5
-        if keys[pygame.K_s] and self.y_pong1 < self.WINDOW_HEIGHT - self.BLOCSIZE*3:
-            self.y_pong1 += 5
-        if keys[pygame.K_UP] and self.y_pong2 > 0:
-            self.y_pong2 -= 5
-        if keys[pygame.K_DOWN] and self.y_pong2 < self.WINDOW_HEIGHT - self.BLOCSIZE*3:
-            self.y_pong2 += 5
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and self.pad1[1] > 0:
+            self.pad1[1] -= 5
+        if keys[pygame.K_s] and self.pad1[1] < self.WINDOW_HEIGHT - self.BLOCSIZE*3:
+            self.pad1[1] += 5
+        if keys[pygame.K_UP] and self.pad2[1] > 0:
+            self.pad2[1] -= 5
+        if keys[pygame.K_DOWN] and self.pad2[1] < self.WINDOW_HEIGHT - self.BLOCSIZE*3:
+            self.pad2[1] += 5
 
     def collisions(self):
-        # Ball and pad 1  
-        if self.x_ball_pos == self.x_pong1:    
-            if self.y_ball_pos < self.y_pong1+(20*3):    
-                if self.y_ball_pos+20 > self.y_pong1:
-                    self.x_velocity *= -1          
-        # Ball and pad 2      
-        if self.x_ball_pos+20 == self.x_pong2:    
-            if self.y_ball_pos+20 >  self.y_pong2:
-                if self.y_ball_pos < self.y_pong2+(20*3):
-                    self.x_velocity *= -1
+        # Ball and pad 1 in X
+        if ballRect.colliderect(pad1Rect):
+            self.velocity[0] *= -1
+            self.velocity[1] = random.choice([-1, 1])
+        # Ball and pad 2 in X
+        if ballRect.colliderect(pad2Rect):
+            self.velocity[0] *= -1
+            self.velocity[1] = random.choice([-1, 1])
 
     def checkScreenBoundaries(self):
-        if self.x_ball_pos < 0:
-                self.x_velocity *= -1
-                self.y_velocity *= -1
-                self.score2+=1
-                
-        if self.x_ball_pos+20 > self.WINDOW_WIDTH:    
-            self.x_velocity *= -1
-            self.y_velocity *= -1
-            self.score1+=1
-            
-        if self.y_ball_pos < 0 or self.y_ball_pos+20 > self.WINDOW_HEIGHT:   
-            self.x_velocity *= -1
-            self.y_velocity *= -1
+        if self.ball[0] < -200:
+            self.ball = [200, 200]
+            self.pad1 = [0, 180]
+            self.pad2 = [410, 180]
+            self.start = False
+            self.scores[1] += 1
+            self.begin_text = 'Press SPACE to start'
 
-    def endGame(self):
-        if self.score1 == 100:
-            self.SCREEN.fill('Black')
-            winner_surface = self.my_font.render('Winner: Player 1' + ' SCORE:' + str(self.score1), False, 'White')
-            self.SCREEN.blit(winner_surface, (0, 0))
-            pygame.display.update()
-            pygame.time.wait(1000)
-            self.score1 = 0
-            self.score2 = 0
-            self.x_ball_pos = 200
-            self.y_ball_pos = 200
-        
-        if self.score2 == 100:
-            
-            self.SCREEN.fill('Black')
-            winner_surface = self.my_font.render('Winner: Player 2' + ' SCORE:' + str(self.score2), False, 'White')
-            
-            self.SCREEN.blit(winner_surface, (0, 0))
-            pygame.display.update()
-            pygame.time.wait(1000)
-            self.score1 = 0
-            self.score2 = 0
-            self.x_ball_pos = 200
-            self.y_ball_pos = 200
+        if self.ball[0]+20 > self.WINDOW_WIDTH+200:
+            self.ball = [200, 200]
+            self.pad1 = [0, 180]
+            self.pad2 = [410, 180]
+            self.start = False
+            self.scores[0] += 1
+            self.begin_text = 'Press SPACE to start'
+
+        if self.ball[1] < 0 or self.ball[1]+20 > self.WINDOW_HEIGHT:
+            self.velocity[0] *= -1
+            self.velocity[1] *= -1
+
+    def endGame(self, score, player):
+        self.SCREEN.fill('Black')
+        winner_surface = self.my_font.render(
+            'Winner: ' + player + ' SCORE:' + str(score), False, 'White')
+        self.SCREEN.blit(winner_surface, (0, 0))
+        pygame.display.update()
+        pygame.time.wait(2000)
+        self.scores[0] = 0
+        self.scores[1] = 0
+        self.ball[0] = 200
+        self.ball[1] = 200
 
 
 game = Game()
